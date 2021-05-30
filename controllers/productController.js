b047d6c824a97a2154e8f878c0b2ae106e2d10ab
Katerina Ilieva <61324205+Kativa7@users.noreply.github.com>
2021-05-30 15:53:54 +0300

    Add files via upload

---
+++ controllers/productController.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("home", {title: 'Browse'});
+});
+
+router.get("/create", (req, res) => {
+  res.render("create", { title: 'Create'});
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const config = require("./config/config");
-const expressConfig = require('./config/express')
+const routes = require('./routes');
 const app = express();
 
+const expressConfig = require('./config/express')
 expressConfig(app)
 
-app.get("/", (req, res) => {
-  res.render('home',{layout: false})
-});
+app.use(routes);
 
 app.listen(config.PORT, () =>
   console.log(`Server is running on port ${config.PORT}`)
